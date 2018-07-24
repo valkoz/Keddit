7a126891dbd030fb66126f5366f248d543d3f39d
valkoz <kozlov.valent@gmail.com>
2018-07-24 22:30:04 +0300

    add rxJava

--- app/src/main/java/ru/moscow/valkoz/keddit/NewsFragment.kt
+++ app/src/main/java/ru/moscow/valkoz/keddit/NewsFragment.kt
@@ -1,18 +1,21 @@
 package ru.moscow.valkoz.keddit
 
 import android.os.Bundle
-import android.support.v4.app.Fragment
+import android.support.design.widget.Snackbar
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import kotlinx.android.synthetic.main.news_fragment.*
-import ru.moscow.valkoz.keddit.commoms.RedditNewsItem
+import ru.moscow.valkoz.keddit.commoms.RxBaseFragment
 import ru.moscow.valkoz.keddit.commoms.extensions.inflate
+import ru.moscow.valkoz.keddit.features.news.NewsManager
 import ru.moscow.valkoz.keddit.features.news.adapter.NewsAdapter
+import rx.schedulers.Schedulers
 
-class NewsFragment : Fragment() {
+class NewsFragment : RxBaseFragment() {
 
+    private val newsManager by lazy { NewsManager() }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return container?.inflate(R.layout.news_fragment)
@@ -23,23 +26,25 @@ class NewsFragment : Fragment() {
 
         news_list.setHasFixedSize(true)
         news_list.layoutManager = LinearLayoutManager(context)
-
         initAdapter()
 
         if (savedInstanceState == null) {
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i,
-                        1457207701L - i * 200,
-                        "https://picsum.photos/200/200?image=$i",
-                        "url"
-                ))
-            }
-            (news_list.adapter as NewsAdapter).addNews(news)
+            requestNews()
         }
+    }
+
+    private fun requestNews() {
+        val subscription = newsManager.getNews()
+                .subscribeOn(Schedulers.io())
+                .subscribe (
+                        { retrievedNews ->
+                            (news_list.adapter as NewsAdapter).addNews(retrievedNews)
+                        },
+                        { e ->
+                            Snackbar.make(news_list, e.message ?: "", Snackbar.LENGTH_LONG).show()
+                        }
+                )
+        subscriptions.add(subscription)
     }
 
     private fun initAdapter() {
---
+++ app/src/main/java/ru/moscow/valkoz/keddit/features/news/NewsManager.kt
@@ -0,0 +1,27 @@
+package ru.moscow.valkoz.keddit.features.news
+
+import ru.moscow.valkoz.keddit.commoms.RedditNewsItem
+import rx.Observable
+
+class NewsManager() {
+
+    fun getNews(): Observable<List<RedditNewsItem>> {
+        return Observable.create {
+            subscriber ->
+
+            val news = mutableListOf<RedditNewsItem>()
+            for (i in 1..10) {
+                news.add(RedditNewsItem(
+                        "author$i",
+                        "Title $i",
+                        i, // number of comments
+                        1457207701L - i * 200, // time
+                        "http://lorempixel.com/200/200/technics/$i", // image url
+                        "url"
+                ))
+            }
+            subscriber.onNext(news)
+            subscriber.onCompleted()
+        }
+    }
+}
