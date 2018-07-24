c52fa709f9b182e8a8834fec796dd2aaede51c41
valkoz <kozlov.valent@gmail.com>
2018-07-24 23:00:52 +0300

    add Retrofit2

--- app/src/main/java/ru/moscow/valkoz/keddit/features/news/NewsManager.kt
+++ app/src/main/java/ru/moscow/valkoz/keddit/features/news/NewsManager.kt
@@ -1,27 +1,28 @@
 package ru.moscow.valkoz.keddit.features.news
 
+import ru.moscow.valkoz.keddit.api.RestAPI
 import ru.moscow.valkoz.keddit.commoms.RedditNewsItem
 import rx.Observable
 
-class NewsManager() {
+class NewsManager(private val api: RestAPI = RestAPI()) {
 
-    fun getNews(): Observable<List<RedditNewsItem>> {
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
         return Observable.create {
             subscriber ->
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
 
-            val news = mutableListOf<RedditNewsItem>()
-            for (i in 1..10) {
-                news.add(RedditNewsItem(
-                        "author$i",
-                        "Title $i",
-                        i, // number of comments
-                        1457207701L - i * 200, // time
-                        "http://lorempixel.com/200/200/technics/$i", // image url
-                        "url"
-                ))
+            if (response.isSuccessful) {
+                val news = response.body()?.data?.children?.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
             }
-            subscriber.onNext(news)
-            subscriber.onCompleted()
         }
     }
 }
