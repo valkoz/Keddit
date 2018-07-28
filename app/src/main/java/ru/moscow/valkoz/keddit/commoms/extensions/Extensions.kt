024ac8008312e05f36c61b5b6e2c8907ec25b054
valkoz <kozlov.valent@gmail.com>
2018-07-28 09:40:00 +0300

    runtime changes handling support

--- app/src/main/java/ru/moscow/valkoz/keddit/commoms/extensions/Extensions.kt
+++ app/src/main/java/ru/moscow/valkoz/keddit/commoms/extensions/Extensions.kt
@@ -2,6 +2,8 @@
 
 package ru.moscow.valkoz.keddit.commoms.extensions
 
+import android.os.Parcel
+import android.os.Parcelable
 import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
@@ -22,3 +24,10 @@ fun ImageView.loadImg(imageUrl: String) {
         Picasso.with(context).load(imageUrl).into(this)
     }
 }
+
+inline fun <reified T : Parcelable> createParcel(
+        crossinline createFromParcel: (Parcel) -> T?): Parcelable.Creator<T> =
+        object : Parcelable.Creator<T> {
+            override fun createFromParcel(source: Parcel): T? = createFromParcel(source)
+            override fun newArray(size: Int): Array<out T?> = arrayOfNulls(size)
+        }
--- app/src/main/java/ru/moscow/valkoz/keddit/features/news/NewsFragment.kt
+++ app/src/main/java/ru/moscow/valkoz/keddit/features/news/NewsFragment.kt
@@ -13,9 +13,14 @@ import ru.moscow.valkoz.keddit.commoms.RedditNews
 import ru.moscow.valkoz.keddit.commoms.RxBaseFragment
 import ru.moscow.valkoz.keddit.commoms.extensions.inflate
 import ru.moscow.valkoz.keddit.features.news.adapter.NewsAdapter
+import rx.android.schedulers.AndroidSchedulers
 import rx.schedulers.Schedulers
 
 class NewsFragment : RxBaseFragment() {
+
+    companion object {
+        private val KEY_REDDIT_NEWS = "redditNews"
+    }
 
     private var redditNews: RedditNews? = null
     private val newsManager by lazy { NewsManager() }
@@ -27,23 +32,36 @@ class NewsFragment : RxBaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        news_list.setHasFixedSize(true)
-        val linearLayout = LinearLayoutManager(context)
-
-        news_list.layoutManager = linearLayout
-        news_list.clearOnScrollListeners()
-        news_list.addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
+        news_list.apply {
+            setHasFixedSize(true)
+            val linearLayout = LinearLayoutManager(context)
+            layoutManager = linearLayout
+            clearOnScrollListeners()
+            addOnScrollListener(InfiniteScrollListener({ requestNews() }, linearLayout))
+        }
 
         initAdapter()
 
-        if (savedInstanceState == null) {
+        if (savedInstanceState != null && savedInstanceState.containsKey(KEY_REDDIT_NEWS)) {
+            redditNews = savedInstanceState.get(KEY_REDDIT_NEWS) as RedditNews
+            (news_list.adapter as NewsAdapter).clearAndAddNews(redditNews!!.news)
+        } else {
             requestNews()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val news = (news_list.adapter as NewsAdapter).getNews()
+        if (redditNews != null && news.size > 0) {
+            outState.putParcelable(KEY_REDDIT_NEWS, redditNews?.copy(news = news))
         }
     }
 
     private fun requestNews() {
         val subscription = newsManager.getNews(redditNews?.after ?: "")
                 .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
                 .subscribe (
                         { retrievedNews ->
                             redditNews = retrievedNews
